8071e3c2130e18b814c3b59ec1663cccf64c0a86
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-10-29 14:09:56 +0900

    2021-10-29 백엔드 게시판 수정

--- source/com/koreait/server/BoardDAO.java
+++ source/com/koreait/server/BoardDAO.java
@@ -23,4 +23,28 @@ public class BoardDAO {
         }
         return 0;
     }
+
+    public static int updBoard(BoardVO param) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        String sql = " UPDATE t_board " +
+                    " SET title = ? " +
+                    " , ctnt = ? " +
+                    " , writer = ? " +
+                    " WHERE iboard = ? ";
+        try {
+            con = DbUtils.getCon();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, param.getTitle());
+            ps.setString(2, param.getCtnt());
+            ps.setString(3, param.getWriter());
+            ps.setInt(4, param.getIboard());
+            return ps.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            DbUtils.close(con, ps);
+        }
+        return 0;
+    }
 }
---
+++ source/com/koreait/server/UpdBoardServlet.java
@@ -0,0 +1,28 @@
+package com.koreait.server;
+
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/upd")
+public class UpdBoardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String json = Utils.getJson(req);
+        Gson gson = new Gson();
+        BoardVO param = gson.fromJson(json, BoardVO.class);
+        int result = BoardDAO.updBoard(param);
+        System.out.println("result : " + result);
+
+    }
+}
